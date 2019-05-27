47aa291f42614a936080351ab3e396d361255410
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-27 00:16:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 2
 def valid_move?(board, index)
-  if board[index].between?(0, 8) && !position_taken?
+  if index.between?(0, 8) && !position_taken?
     return true
   else
     return false
